c6b466bc9f2fbd74dfdaf00c7e00cf31fe686edb
GarudaFX <franfra10j@gmail.com>
2024-02-20 10:57:50 +0800

    docs: initialized vercel.json

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useMedia } from '@/hooks/useMedia'
+// import { useMedia } from '@/hooks/useMedia'
 
 const Landing: React.FC = () => {
- const isMobile = useMedia('(max-width: 768px)')
+ // const isMobile = useMedia('(max-width: 768px)')
  return <div className="font-main">Landing Page</div>
 }
 
