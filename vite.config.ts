cbec1d3510cfbabd9a63ca3d66fac034f11970c3
GarudaFX <franfra10j@gmail.com>
2024-04-11 19:52:24 +0800

    added pwa for desktop app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
+ plugins: [
+  react(),
+  VitePWA({
+   registerType: 'autoUpdate',
+   devOptions: {
+    enabled: false
+   },
+   includeAssets: ['LargeTile.scale-125.png'],
+   manifest: {
+    name: 'One Siargao',
+    short_name: 'GIS',
+    description:
+     'Best Routing GIS for finding the best tourist spots in Siargao according to your availibity',
+    theme_color: '#1154FF',
+    background_color: '#1154FF',
+    start_url: '/gis',
+    icons: [
+     {
+      src: '/LargeTile.scale-125.png',
+      sizes: '125x125',
+      type: 'image/png'
+     }
+    ],
+    display: 'standalone'
+   }
+  })
+ ],
  resolve: {
   alias: {
    '@': '/src'
